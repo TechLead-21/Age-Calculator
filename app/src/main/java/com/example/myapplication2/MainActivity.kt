5628631ca924e416df18b28dbdc5dda9383e2b9d
Jayesh Tembhekar <jtembhekar2@gmail.com>
2020-06-02 16:45:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication2/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tvAge.setText("");
+
+    }
+
+    public fun calculateAge(view:View){
+        val dob:String = etBirth.text.toString()
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        val age = currentYear - dob.toInt()
+        tvAge.setText("Your Age is $age")
+    }
+}
